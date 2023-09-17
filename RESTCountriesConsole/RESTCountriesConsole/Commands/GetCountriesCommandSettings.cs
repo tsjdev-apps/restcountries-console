a37481beded3c440cd6731a2726a37842e0d0521
Sebastian Jensen <apps@tsjdev-apps.de>
2023-09-17 08:24:19 +0200

    Add GetCountriesCommandSettings.

---
+++ RESTCountriesConsole/RESTCountriesConsole/Commands/GetCountriesCommandSettings.cs
@@ -0,0 +1,15 @@
+﻿using Spectre.Console.Cli;
+using System.ComponentModel;
+
+namespace RESTCountriesConsole.Commands;
+
+internal class GetCountriesCommandSettings : CommandSettings
+{
+    [CommandArgument(0, "[operation]")]
+    [Description("The operation to get the countries.")]
+    public string? Operation { get; set; }
+
+    [CommandOption("-p|--parameter")]
+    [Description("Specifies the parameter for the operation.")]
+    public string? Parameter { get; set; }
+}
